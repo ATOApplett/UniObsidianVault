0fc494d6adb68babb505ce4d51e6b92df01798d1
Alex Ong <ongax0912@gmail.com>
2026-07-31 15:18:19 +1000

    vault backup: 2026-07-31 15:18:19

--- 2026Sem2/CAB403/Assignments/A1/dbl_vector.c
+++ 2026Sem2/CAB403/Assignments/A1/dbl_vector.c
@@ -2,24 +2,39 @@
 #include <stdlib.h>
 #include "dbl_vector.h"
 
+// Definitions
+#define DV_INITIAL_CAPACITY 4
+#define DV_GROWTH_FACTOR 1.25
+// function definitions
+void dv_init (dbl_vector_t* vec);
+//struct
 struct dbl_vector {
     size_t size;
     size_t capacity;
     double* data;
-    #define DV_INITIAL_CAPACITY 4
-    #define DV_GROWTH_FACTOR 1.25
-} dbl_vector_t;
+};
 
 void dv_init( dbl_vector_t* vec ) {
-
+    //double* new_vector = malloc(DV_INITIAL_CAPACITY * sizeof(double));
+    vec->size = 0;
+    vec->capacity = DV_INITIAL_CAPACITY;
+    vec->data = malloc(vec->capacity * sizeof(double));
 }
 
 void dv_ensure_capacity( dbl_vector_t* vec, size_t new_size ) {
-    // INSERT SOLUTION HERE
+    if (vec->size != new_size) {
+        size_t new_capacity = vec->capacity * DV_GROWTH_FACTOR;
+        if (new_size <= new_capacity) {
+            new_capacity = new_size;
+        }
+        double* new_vector = realloc(vec->data, new_capacity * sizeof(double));
+        vec->data = new_vector;
+        vec->capacity = new_capacity;
+    }
 }
 
 void dv_destroy( dbl_vector_t* vec ) {
-    // INSERT SOLUTION HERE
+    
 }
 
 void dv_copy( dbl_vector_t* vec, dbl_vector_t* dest ) {
