5447bb61e0f33e754e20202525cc6fdaf68dce39
Alex Ong <ongax0912@gmail.com>
2026-08-17 14:28:42 +1000

    vault backup: 2026-08-17 14:28:42

--- 2026Sem2/CAB403/Assignments/A1/dbl_vector.c
+++ 2026Sem2/CAB403/Assignments/A1/dbl_vector.c
@@ -34,39 +34,66 @@ void dv_ensure_capacity( dbl_vector_t* vec, size_t new_size ) {
 }
 
 void dv_destroy( dbl_vector_t* vec ) {
-    
+    free(vec->data);
+    vec->data = NULL;
+    vec->size = 0;
+    vec->capacity = 0;
 }
 
 void dv_copy( dbl_vector_t* vec, dbl_vector_t* dest ) {
-    // INSERT SOLUTION HERE
+    dv_ensure_capacity( dest, vec->size );
+    for ( size_t i = 0; i < vec->size; i++ ) {
+        dest->data[i] = vec->data[i];
+    }
+    dest->size = vec->size;
 }
 
 void dv_clear( dbl_vector_t* vec ) {
-    // INSERT SOLUTION HERE
+    vec->size = 0;
 }
 
 void dv_push( dbl_vector_t* vec, double new_item ) {
-    // INSERT SOLUTION HERE
+    dv_ensure_capacity( vec, vec->size + 1 );
+    vec->data[vec->size] = new_item;
+    vec->size++;
 }
 
 void dv_pop( dbl_vector_t* vec ) {
-    // INSERT SOLUTION HERE
+    if (vec->size > 0) {
+        vec->size--;
+    }
 }
 
 double dv_last( dbl_vector_t* vec ) {
     double result = NAN;
-    // INSERT SOLUTION HERE
+    if (vec->size > 0) {
+        result = vec->data[vec->size - 1];
+    }
     return result;
 }
 
 void dv_insert_at( dbl_vector_t* vec, size_t pos, double new_item ) {
-    // INSERT SOLUTION HERE
+    if (pos < vec->size) {
+        dv_ensure_capacity( vec, vec->size + 1 );
+        for ( size_t i = vec->size; i > pos; i-- ) {
+            vec->data[i] = vec->data[i - 1];
+        }
+        vec->data[pos] = new_item;
+        vec->size++;
+    }
 }
 
 void dv_remove_at( dbl_vector_t* vec, size_t pos ) {
-    // INSERT SOLUTION HERE
+    if (pos < vec->size) {
+        for ( size_t i = pos; i < vec->size - 1; i++ ) {
+            vec->data[i] = vec->data[i + 1];
+        }
+        vec->size--;
+    }
 }
 
 void dv_foreach( dbl_vector_t* vec, void (*callback)(double, void*), void* info ) {
-    // INSERT SOLUTION HERE
+    for ( size_t i = 0; i < vec->size; i++ ) {
+        callback( vec->data[i], info );
+    }
 }
